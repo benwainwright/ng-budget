deca23ae7a966d95c40236c858bdf52deb46be6d
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-05-30 20:24:18 +0100

    initial commit

---
+++ src/app/settings.service.spec.ts
@@ -0,0 +1,7 @@
+import { SettingsService } from './settings.service';
+
+describe('settings service', () => {
+  it('should be instantiated without error', () => {
+    new SettingsService();
+  });
+});
