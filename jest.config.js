360b939ad6a8e373705ec7bbbd5889b7a4bfb9ae
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-01 19:52:55 +0100

    fixed tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,19 @@
 // jest.config.js
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    globalSetup: 'jest-preset-angular/global-setup',
-    "globals": {
-        "ts-jest": {
-          "tsconfig": "<rootDir>/tsconfig.spec.json",
-          "stringifyContentPathRegex": "\\.html$",
-          "useESM": true
-        }
-      }
-  
+  preset: "jest-preset-angular",
+  collectCoverageFrom: ["<rootDir>/src/**"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/main.ts",
+    "<rootDir>/src/environments/.*",
+    "<rootDir>/src/index.html",
+  ],
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  globalSetup: "jest-preset-angular/global-setup",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+      useESM: true,
+    },
+  },
 };
